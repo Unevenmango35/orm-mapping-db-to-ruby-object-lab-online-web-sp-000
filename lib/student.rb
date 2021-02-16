0e5c23b77de547a5945a66020262d541e2750b75
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-16 21:23:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,14 +114,14 @@ class Student
     end.first
    end
 
-   def self.all_students_in_grade_X(X)
+   def self.all_students_in_grade_X(grade)
      sql = <<-SQL
       SELECT *
       FROM students
       WHERE grade = ?
     SQL
 
-    DB[:conn].execute(sql, X).map do |row|
+    DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
     end
    end
