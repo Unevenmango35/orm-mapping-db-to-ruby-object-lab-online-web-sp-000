fd6040a10fd7b89844715caa2e6ecc6db7604411
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-16 21:20:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,7 @@ class Student
     end
   end
 
-   def self.first_X_students_in_grade_10(n)    
+   def self.first_X_students_in_grade_10(n)
      sql = <<-SQL
       SELECT *
       FROM students
@@ -100,7 +100,7 @@ class Student
     end
    end
 
-   def self.first_X_students_in_grade_10    
+   def self.first_X_students_in_grade_10
      sql = <<-SQL
       SELECT *
       FROM students
