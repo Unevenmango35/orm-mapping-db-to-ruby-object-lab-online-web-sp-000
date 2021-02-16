23cc1827e2e07c6ab3c4c160cd23a483694b04fe
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-16 21:22:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,4 +114,16 @@ class Student
     end.first
    end
 
+   def self.all_students_in_grade_X(X)
+     sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, n).map do |row|
+      self.new_from_db(row)
+    end
+   end
+
 end
