bd347b22afb39deef60839149d826bb5767e336e
Rosie Choi <rosiechoimanso@gmail.com>
2021-02-16 21:01:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,9 +72,11 @@ class Student
   def self.all_students_in_grade_12
     sql = <<-SQL
     SELECT *
-    FROM students WHERE grade =< 11
+    FROM students WHERE grade < 12
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
